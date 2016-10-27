1ddee44cdb052e28c4380762ef55eb98947dd750
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-10-26 20:51:02 -0700

    dp calc skeletion

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ function Region(name, type, averageList, sq) {
   this.type = [];
   this.averageList = averageList;
   this.sq = sq;
+  this.dp = 0;
   houseData.push(this);
 }
 
+//TODO: GET DP WORKING
+Region.prototype.createDP = function() {
+  for (var i = 0; i < houseData.length; i++) {
+    this.avergerageList[i] * 0.10;
+    this.dp += this.averageList[i];
+  }
+};
+Region.prototype.createDP();
 new Region('North', 'House', 480000, 1200);
 new Region('East', 'Condo', 439500, 1175);
 new Region('West', 'Condo', 438000, 1171);
