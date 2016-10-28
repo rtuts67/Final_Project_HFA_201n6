1911fca3e809d9145a8681af11b746a60db4ffe2
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-10-27 21:41:27 -0700

    got table to work, need to add css to table

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-var tableHeader = ['Region','Type','Average List Price', 'Square Ft'];
+var tableHeader = ['Region','Type','Average List Price', 'Square Ft', 'Downpayment'];
 
 var houseData = [];
 
@@ -45,16 +45,39 @@ function makeFirstRow() {
   for (var i = 0; i < tableHeader.length; i++) {
     var headers = document.createElement ('th');
     headers.textContent = tableHeader[i];
-    firstRow.appendChild(headers)
+    firstRow.appendChild(headers);
   }
 };
 makeFirstRow();
 
+function makeOtherRows() {
+  for (var x = 0; x < houseData.length; x++) {
+    var tableRows = document.getElementById('table');
 
-  //var makeRow = document.createElement('tr');
-  //makeRow.textContent = firstCell;
+    var makeRows = document.createElement('tr');
 
-  //var firstCell = document.createElement('td');
-  //firstCell.textContent = reg.name;
-//  makeRow.appendChild(firstCell)
-//}
+    var firstCell = document.createElement('td');
+    firstCell.textContent = houseData[x].name;
+    makeRows.appendChild(firstCell);
+    console.log(houseData[x].name);
+
+    var secondCell = document.createElement('td');
+    secondCell.textContent = houseData[x].type;
+    makeRows.appendChild(secondCell);
+
+    var thirdCell = document.createElement('td');
+    thirdCell.textContent = houseData[x].averageList;
+    makeRows.appendChild(thirdCell);
+
+    var fourthCell = document.createElement('td');
+    fourthCell.textContent = houseData[x].sq;
+    makeRows.appendChild(fourthCell);
+
+    var fifthCell = document.createElement('td');
+    fifthCell.textContent = houseData[x].dp;
+    makeRows.appendChild(fifthCell);
+
+    tableRows.appendChild(makeRows);
+  }
+};
+makeOtherRows();
