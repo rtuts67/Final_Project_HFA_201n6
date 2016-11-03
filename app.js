d8ca2e4b2f4526ae38c98046c8fea04c11734ad2
tommytnez <tommytnez@yahoo.com>
2016-11-02 20:49:52 -0700

    fixed Meagans localS

--- app.js
+++ app.js
@@ -143,14 +143,32 @@ makeOtherRows();
 var theToggle = document.getElementById('toggle');
 
 function hasClass(elem, className) {
+
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+
   return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+
 }
 function addClass(elem, className) {
+
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+
   if (!hasClass(elem, className)) {
     elem.className += ' ' + className;
   }
 }
 function removeClass(elem, className) {
+
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+
   var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
   if (hasClass(elem, className)) {
     while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
@@ -158,8 +176,25 @@ function removeClass(elem, className) {
     }
     elem.className = newClass.replace(/^\s+|\s+$/g, '');
   }
+
 }
 function toggleClass(elem, className) {
+
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+
   var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
   if (hasClass(elem, className)) {
     while (newClass.indexOf(" " + className + " ") >= 0 ) {
